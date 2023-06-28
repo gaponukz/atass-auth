01c0b7d8a5af860649135c67f8b2316d70cf6a2b
gaponukz <gaponukz54@gmail.com>
2023-06-28 23:03:44 +0300

    Enable e2e tests

---
+++ cmd/for_e2e/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"auth/src/controller"
+	"auth/src/registration"
+	"auth/src/resetPassword"
+	"auth/src/settings"
+	"auth/src/storage"
+	"auth/src/web"
+)
+
+func main() {
+	databaseFilename := "test.json"
+	err := os.WriteFile(databaseFilename, []byte("[]"), 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		os.Remove(databaseFilename)
+	}()
+
+	settings := settings.NewDotEnvSettings().Load()
+	futureUserStor := storage.NewRedisTemporaryStorage("localhost:6379", 1*time.Minute, "register")
+	resetPassStor := storage.NewRedisTemporaryStorage("localhost:6379", 1*time.Minute, "reset")
+	userStorage := storage.NewUserJsonFileStorage(databaseFilename)
+
+	server := web.SetupTestServer(
+		controller.Controller{
+			Storage:  userStorage,
+			Settings: settings,
+			RegistrationService: registration.NewRegistrationService(
+				userStorage,
+				futureUserStor,
+				func(gmail string, key string) error { return nil },
+				func() string { return "12345" },
+			),
+			ResetPasswordService: resetPassword.NewResetPasswordService(
+				userStorage,
+				resetPassStor,
+				func(gmail string, key string) error { return nil },
+				func() string { return "12345" },
+			),
+		},
+	)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
+}
