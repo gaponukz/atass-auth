461c9851da8e611beaf3d5834dc0016937f6513a
gaponukz <gaponukz54@gmail.com>
2023-08-01 15:15:39 +0300

    Add base

---
+++ src/logger/log_add_route_decorator.go
@@ -0,0 +1,27 @@
+package logger
+
+import "fmt"
+
+type routesService interface {
+	AddRoute(userID, routeID string) error
+}
+
+type logger interface {
+	Error(string)
+	Info(string)
+}
+
+type logRoutesService struct {
+	s routesService
+	l logger
+}
+
+func (s logRoutesService) AddRoute(userID, routeID string) error {
+	err := s.s.AddRoute(userID, routeID)
+	if err != nil {
+		s.l.Error(fmt.Sprintf("Can not add route %s to user %s: %v", routeID, userID, err))
+	}
+
+	s.l.Info(fmt.Sprintf("Add route %s to user %s", routeID, userID))
+	return nil
+}
